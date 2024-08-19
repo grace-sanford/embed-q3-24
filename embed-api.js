a00459e9ae9c0cb98ddbc98481c8f6e5b6208aad
Grace Sanford <gracesanford@protonmail.com>
2024-08-19 19:57:07 -0400

    feat(env)

--- embed-api.js
+++ embed-api.js
@@ -1,33 +1,3 @@
-// // SIGMA SERVER-SIDE EMBED API - PUBLIC QUICKSTART
-
-// // USE EXPRESS WEB FRAMEWORK:
-// var express = require('express');
-// var app = express();
-
-// // FETCH PATH FOR FOLDER CONTAINING HTML PAGE WE ARE EMBEDDING INTO:
-// app.get('/', function(request, response) 
-// {
-// response.sendFile(__dirname + '/index.html');
-// }
-// );
-
-// // DEFINE A ROUTE HANDLER FOR GET REQUESTS TO GIVEN URL:
-// app.get('/api/generate-embed-url', generate_embed_url);
-
-// // CALL FUNCTION AND SET ANY PARAMETERS
-// function generate_embed_url(request, response) 
-// {
-// 	// SET SIGMA EMBED URL VALUE:
-// 	// THIS VALUE IS CALLED "PUBLIC LINK" IN SIGMA PORTAL:
-// 	const EMBED_PATH = "https://app.sigmacomputing.com/embed/t1VWJDb4675r5Uina5Mle&:nonce=d1302425-6b61-4d93-a412-07c6d58af6a7&:time=1724108453&:session_length=3600&:external_user_id=1&:ua_Attr1=3&:theme=Dark&:mode=userbacked&:client_id=31c0d37dd9cffe984b412aa340509085deecce89940412a7118d1f60d992302c&:hide_folder_navigation=true&:menu_position=bottom&:email=gracesanford@protonmail.com&:account_type=embedUser&:hide_run_as_recipient=true&:external_user_team=Grace%20Test%20Team&:ua_Attr1=3&:signature=ff3c71fa1833cd93ecf81a8b13ccb8bf89726c5303a7144097a7f80d83ce0f33"; 
-
-// 	// SEND URL TO CLIENT:
-// 	response.status(200).send({url:EMBED_PATH});
-// }
-
-// //START EXPRESS SERVER LISTENING ON PORT 3001:
-// app.listen(3000);
-
 // SIGMA SERVER-SIDE EMBED API - SECURE QUICKSTART
 
 // 1: Require necessary Node.js modules
@@ -38,10 +8,11 @@ const crypto = require('crypto');
 const app = express();
 
 // 3: Manually set your configuration variables here (example values shown)
-const EMBED_PATH = 'https://app.sigmacomputing.com/embed/4lzk9YKJO41vLKn2ZqXPb2';
-const CLIENT_ID = '31c0d37dd9cffe984b412aa340509085deecce89940412a7118d1f60d992302c'
-const EMBED_SECRET = '8089aa202308a6730aacf119a43e6e4bf5885a3b7f2b094776a9e43fbe7b3ed7f82fc6db1061f01642201cff418028b80693662a0240cf1709cf003bdd72e39f';
-const PORT = 3000; // Feel free to change the port number as needed
+//TODO: mask these
+const EMBED_PATH = process.env.EMBED_PATH;
+const CLIENT_ID = process.env.CLIENT_ID;
+const EMBED_SECRET = process.env.EMBED_SECRET;
+const PORT = process.env.PORT; // Feel free to change the port number as needed
 
 // 4: Server Setup
 app.get('/', (req, res) => {
