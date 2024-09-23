41134f824f4f884eca371b599cf6da2f4b8fd87e
Grace Sanford <gracesanford@protonmail.com>
2024-09-23 18:01:02 -0400

    feat(embed-api): more params

--- embed-api.js
+++ embed-api.js
@@ -43,14 +43,57 @@ app.get('/api/generate-embed-url', (req, res) => {
         // 6: Construct required search parameters
         searchParams += `&:client_id=${CLIENT_ID}`;
         searchParams += '&:email=gracesanford@protonmail.com';
-        searchParams += '&:external_user_id=g1';
+        searchParams += '&:external_user_id=1';
         searchParams += '&:external_user_team=Grace%20Test%20Embed%20Team';
         searchParams += '&:account_type=Creator';
         searchParams += '&:mode=userbacked';
         searchParams += '&:session_length=600';
 		searchParams += '&:ua_grace-edit-attr-test=value';
         searchParams += `&:time=${Math.floor(new Date().getTime() / 1000)}`;
+		//-- Additional parameters start --//
+		//SET A VALUE FOR disable_mobile_view. Default=no.
+		searchParams += '&:disable_mobile_view=yes';
+		//SET A VALUE FOR language. Default=en List of supported languages: https://help.sigmacomputing.com/docs/manage-workbook-localization#supported-languages-and-locales
+		searchParams += '&:lng=fa-ca';
+		//SET A VALUE FOR first_name. Default=Embed.
+		// searchParams += '&:first_name=Bob';
+		//SET A VALUE FOR hide_folder_navigation. Default=false.
+		searchParams += '&:hide_folder_navigation=true';
+		//HIDE THE MENU IN BOTTOM-LEFT CORNER FROM EMBED USERS. DEFAULT=false
+		searchParams += '&:hide_menu=true';
+		//HIDE THE "SCHEDULE NOW" OPTION IN THE BOTTOM-LEFT CORNER FROM EMBED USERS. DEFAULT=false
+		searchParams += '&:hide_schedule=true';
+		//HIDE THE "SEND NOW" OPTION IN THE BOTTOM-LEFT CORNER FROM EMBED USERS. DEFAULT=false
+		searchParams += '&:hide_send=true';
+		//HIDE SHEET INTERACTIONS OPTION FROM EMBED USERS. DEFAULT=false
+		searchParams += '&:hide_sheet_interactions=false';
+		//SET A VALUE FOR hide_tooltip. Default=false.
+		searchParams += '&:hide_tooltip=true';
+		//SET A VALUE FOR hide_workbook_name. Default=false.
+		searchParams += '&:hide_workbook_name=true';
+		//SET A VALUE FOR last_name. Default=User.
+		// searchParams += '&:last_name=Smith';
+		//SET A COLOR FOR THE BACKGROUND ON LOADING AND ERROR SCREENS
+		searchParams += '&:loading_bg=FF0000';
+		//SET A COLOR FOR THE FONT ON LOADING AND ERROR SCREENS 
+		searchParams += '&:loading_text=FFFFFF';
+		//SET THE POSITION OF THE TOOLBAR. DEFAULT = "bottom".
+		searchParams += '&:menu_position=top';
+		//ENABLE JAVASCRIPT EVENT FOR responsive_height. DEFAULT = "false".
+		searchParams += '&:responsive_height=true';
+		//SHOW FOOTER ON PAGE OR NOT. DEFAULT = "true".
+		searchParams += '&:show_footer=false';
+		//CHANGE THEME SIGMA WILL USE FOR THE EMBED. DEFAULT = "Light".
+		searchParams += '&:theme=Dark';
+		searchParams += '&:ua_Region=West';
+		//Sets the workbook creator's name (instead of email) in embed menus and system-generated emails. DEFAULT=
+		// searchParams += '&:use_user_name=Bob Smith';
+		//PASSES A SECURE TOKEN TO SIGMA THAT AUTHENTICATES CONNECTIONS USING OAUTH
+		// searchParams += '&:oauth_token={token}';
 		// searchParams += `&Roman-Numeral-CONTROL=I`;
+
+		//-- Additional parameters end --//
+		
 
         // 7: Construct the URL with search parameters and generate a signature
         const URL_WITH_SEARCH_PARAMS = EMBED_PATH + searchParams;
