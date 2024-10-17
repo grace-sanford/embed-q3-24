496902a209d6f589adf746d67c336c08f20a14c8
Grace Sanford <gracesanford@protonmail.com>
2024-10-17 17:30:02 -0400

    feat(react-sdk-home)

--- embed-sdk-react/docs/basic-examples/components/main-nav.tsx
+++ embed-sdk-react/docs/basic-examples/components/main-nav.tsx
@@ -11,18 +11,21 @@ export function MainNav() {
   // Determine which logo to display and what header text to use based on the pathname
   const isBasicExample = pathname.includes("/examples/basic-example");
   const LogoComponent = isBasicExample ? PlugsLogo : SigmaIcon;
-  const headerText = isBasicExample ? "Sample Parent Application with Sigma Embed" : "Welcome to the Embedding SDK for React";
+  const headerText = isBasicExample ? "Sample Parent Application with Sigma Embed" : "Welcome to Embedding Notepad";
 
   return (
-    <div className="flex items-center space-x-4 p-4">
-      {/* Conditionally render the button only for the PlugsLogo */}
-      {isBasicExample && (
-        <Link href="/" className="text-white bg-[rgb(23,69,68)] hover:bg-[rgb(23,69,68)]/90 font-bold py-2 px-4 rounded">
-          Go Back
-        </Link>
-      )}
-      <LogoComponent size={50} />  {/* Ensure logo size is appropriate */}
-      <h2 className="font-bold text-2xl whitespace-nowrap my-auto">{headerText}</h2>
-    </div>
+    <>
+      <div className="flex items-center space-x-4 p-4">
+        {/* Conditionally render the button only for the PlugsLogo */}
+        {isBasicExample && (
+          <Link href="/" className="text-white bg-[rgb(23,69,68)] hover:bg-[rgb(23,69,68)]/90 font-bold py-2 px-4 rounded">
+            Go Back
+          </Link>
+        )}
+        <LogoComponent size={50} />  {/* Ensure logo size is appropriate */}
+        <h2 className="font-bold text-2xl whitespace-nowrap my-auto">{headerText}</h2>
+      </div>
+      <div>A q3 '24 TSE quarterly project</div>
+    </>
   );
 }
